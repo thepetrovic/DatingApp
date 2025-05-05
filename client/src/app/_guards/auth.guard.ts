33fbe48d0273fafcda554ff362cdc06fdc43ce25
stefan.petrovic1 <p.stefan44035@gmail.com>
2025-05-05 12:26:08 +0200

    End of section 6

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { AccountService } from './../_services/account.service';
+import { CanActivateFn } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService  = inject(AccountService);
+  const toastr = inject(ToastrService);
+
+  if(accountService.currentUser()) {
+    return true;
+  }else {
+    toastr.error('You shall not pass!');
+    return false;
+  }
+};
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,29 +1,35 @@
 import { Component, inject } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule],
+  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive,TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService=inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService)
   model:any = {}
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: _ => {
+        void this.router.navigateByUrl('/members');
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
+
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
